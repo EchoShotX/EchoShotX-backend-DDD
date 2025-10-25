3ea0ce7916277be160feb39bc8260694ab8fac29
hamlsy <dltmddud1122@naver.com>
2025-10-25 19:21:38 +0900

    refactor: #11

--- src/main/java/com/example/echoshotx/video/application/service/VideoService.java
+++ src/main/java/com/example/echoshotx/video/application/service/VideoService.java
@@ -1,10 +1,8 @@
 package com.example.echoshotx.video.application.service;
 
-import com.example.echoshotx.video.application.validator.VideoValidator;
 import com.example.echoshotx.video.domain.entity.ProcessingType;
 import com.example.echoshotx.video.domain.entity.Video;
 import com.example.echoshotx.video.infrastructure.persistence.VideoRepository;
-import com.example.echoshotx.shared.aws.service.AwsS3Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,8 +17,6 @@ import java.time.LocalDateTime;
 public class VideoService {
 
     private final VideoRepository videoRepository;
-    private final AwsS3Service awsS3Service;
-    private final VideoValidator videoValidator;
 
     public Video uploadVideo(Long memberId, String fileName, long fileSize,
                              ProcessingType processingType, String uploadId, String s3Key, LocalDateTime expiresAt) {
